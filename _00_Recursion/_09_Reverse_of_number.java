acf4376def39513e89a78dae44fd049bb6720619
Manish Kumar Dohare <manishdohare0504@gmail.com>
2026-06-15 16:06:12 +0530

    Updated project

--- _00_Recursion/_09_Reverse_of_number.java
+++ _00_Recursion/_09_Reverse_of_number.java
@@ -1,4 +1,5 @@
 public class _09_Reverse_of_number {
+    static int reverse=0;
     void main() {
 
         int num = Integer.parseInt(IO.readln("Enter a number:"));
@@ -9,5 +10,13 @@ public class _09_Reverse_of_number {
     }
 
     public static int reverseNumber(int num) {
-    }
+        
+       if(num==0){
+       return reverse;
+        }
+       reverse= reverse*10+num%10;
+       reverseNumber(num/10);
+       return reverse;
+
+    } 
 }
