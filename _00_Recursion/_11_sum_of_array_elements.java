775f96823b5a951051978c5175e8c0b5b9d40b1d
Manish Kumar Dohare <manishdohare0504@gmail.com>
2026-06-17 23:33:32 +0530

    Your commit message

---
+++ _00_Recursion/_11_sum_of_array_elements.java
@@ -0,0 +1,21 @@
+
+public class _11_sum_of_array_elements {
+
+    public static int findSum(int[] arr, int index) {
+
+        if (index == arr.length) {
+            return 0;
+        }
+
+        return arr[index] + findSum(arr, index + 1);
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        int sum = findSum(arr, 0);
+
+        System.out.println("Sum = " + sum);
+    }
+}
